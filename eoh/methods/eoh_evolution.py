e9e69b2164d5d2ba876aed98d45da0369e0878b8
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-09-26 18:11:13 +0800

    update EvolGraph (need to connect to EC Interfact -- strange name)

--- eoh/methods/eoh_evolution.py
+++ eoh/methods/eoh_evolution.py
@@ -9,6 +9,15 @@ from typing import Optional
 #   - Node: Code, Prompt, Tool  #
 #   - Edge: Not Decided Yet     #
 #################################
+
+
+# Evolution: 
+# - (Hyper-parameter) Meta-Heuristic: Task, FuncName, Input, Output
+# - (get_prompt_xx) Evolution tactic specific prompt templating, used for generating node
+# - (_get_node) Get prompt response from LLM, parse node content (code, tool, prompt) 
+# - (xx) i1/e1/m1/m2/e2 Evolution search on node
+#
+# Missing: Topology search of our graph
         
 class EvolNode(ABC):
     
@@ -40,24 +49,39 @@ class EvolNode(ABC):
     def m2(self, parents: list):
         return self._evolve('m2', parents)
 
-
-
-# Evolution: 
-# - (Hyper-parameter) Meta-Heuristic: Task, FuncName, Input, Output
-# - (get_prompt_xx) Evolution tactic specific prompt templating, used for generating node
-# - (_get_node) Get prompt response from LLM, parse node content (code, tool, prompt) 
-# - (xx) i1/e1/m1/m2/e2 Evolution search on node
-#
-# Missing: Topology search of our graph
-
-
-class Graph:
+class EvolGraph:
     def __init__(self):
-        self.nodes = []
+        self.nodes = {}  # Use a dictionary to store nodes with unique identifiers
         self.edges = []
 
-    def add_node(self, node):
-        self.nodes.append(node)
+    def add_node(self, node_id: str, node: EvolNode):
+        self.nodes[node_id] = node
 
-    def add_edge(self, node1, node2):
-        self.edges.append((node1, node2))
+    def add_edge(self, from_node_id: str, to_node_id: str):
+        if from_node_id in self.nodes and to_node_id in self.nodes:
+            self.edges.append((from_node_id, to_node_id))
+        else:
+            raise ValueError("Both nodes must exist in the graph before adding an edge.")
+
+    def get_node(self, node_id: str) -> Optional[EvolNode]:
+        return self.nodes.get(node_id)
+
+    def get_input_nodes(self, node_id: str) -> list[EvolNode]:
+        return [self.nodes[from_id] for from_id, to_id in self.edges if to_id == node_id]
+
+    def get_output_nodes(self, node_id: str) -> list[EvolNode]:
+        return [self.nodes[to_id] for from_id, to_id in self.edges if from_id == node_id]
+    
+    @classmethod
+    def parse_graph_from_response(cls, response):
+        pass
+
+    def evolve_node(self, node_id: str, method: str):
+        node = self.get_node(node_id)
+        if node:
+            input_nodes = self.get_input_nodes(node_id)
+            if method == 'i1':
+                return node.i1()
+            elif method in ['e1', 'e2', 'm1', 'm2']:
+                return getattr(node, method)(input_nodes)
+        return None
