52e268ef068fdcd12700c7c2a28b6bf2e9570185
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-09-26 13:55:09 +0800

    update preliminary code

---
+++ eoh/methods/eoh_interface_EC.py
@@ -0,0 +1,58 @@
+import numpy as np
+from .eoh_evolution import Evolution
+from joblib import Parallel, delayed
+import concurrent.futures
+
+class InterfaceEC():
+    def __init__(self, pop_size: int, m: int, llm_model: str,
+                 interface_prob, # Special problem class
+                 select,  # Special selection class
+                 n_p: int, timeout: int, use_numba: bool, **kwargs):
+        
+        self.pop_size = pop_size
+        self.interface_eval = interface_prob
+        self.evol = Evolution(llm_model, interface_prob.prompts, **kwargs)
+        self.m = m
+        self.select = select
+        self.n_p = n_p
+        self.timeout = timeout
+        self.use_numba = use_numba
+
+    def check_duplicate(self, population, code):
+        return any(code == ind['code'] for ind in population)
+
+    def _get_alg(self, pop, operator):
+        """ 
+        Generate one offspring using specific operator 
+        - Select parent
+        - Evolve (crossover or mutation)
+        """
+        offspring = {'algorithm': None, 'code': None, 'objective': None, 'other_inf': None}
+        parents = self.select.parent_selection(pop, self.m) if operator != "i1" else None
+        
+        if operator == "i1":
+            offspring['code'], offspring['algorithm'] = self.evol.i1()
+        elif operator in ["e1", "e2", "m1", "m2"]:
+            method = getattr(self.evol, operator)
+            offspring['code'], offspring['algorithm'] = method(parents)
+        
+        return parents, offspring
+
+    def get_offspring(self, pop, operator):
+        p, offspring = self._get_alg(pop, operator)
+        
+        while self.check_duplicate(pop, offspring['code']): # Desperate move for diversity ensurance | External stimulus is necessary
+            p, offspring = self._get_alg(pop, operator)
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(self.interface_eval.evaluate, offspring['code'])
+            fitness = future.result(timeout=self.timeout) # Run Evaluation with TimeOut
+            offspring['objective'] = np.round(fitness, 5)
+
+        return p, offspring
+
+    def get_algorithm(self, pop, operator): # Parallel Genetic Search
+        results = Parallel(n_jobs=self.n_p, timeout=self.timeout+15)(
+            delayed(self.get_offspring)(pop, operator) for _ in range(self.pop_size)
+        )
+        return zip(*results)
