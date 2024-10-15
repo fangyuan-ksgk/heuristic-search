3e5bf269213e736f79ceb22adde9d24ff194c3de
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-10-15 16:22:47 +0800

    update code

--- set.sh
+++ set.sh
@@ -2,3 +2,6 @@ pip install nb-mermaid
 npm install @mermaid-js/mermaid-cli
 # brew install inkscape imagemagick
 # curl -fsSL https://d2lang.com/install.sh | sh -s --
+pip install --upgrade transformers trl huggingface_hub datasets accelerate bitsandbytes peft vllm deepspeed
+MAX_JOBS=4 pip install flash-attn -U --no-build-isolation --force-reinstall
+pip install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.3
