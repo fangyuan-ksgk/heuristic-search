3aa95847afee0aff0a9633f9a2c94c23357be540
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-11-05 15:18:11 +0800

    update set up file for d2 diagram generation on linux environment

---
+++ set.sh
@@ -0,0 +1,8 @@
+pip install nb-mermaid astor
+npm install @mermaid-js/mermaid-cli
+# brew install inkscape imagemagick
+curl -fsSL https://d2lang.com/install.sh | sh -s --
+pip install --upgrade transformers trl huggingface_hub datasets accelerate bitsandbytes peft deepspeed sentence_transformers
+MAX_JOBS=4 pip install flash-attn -U --no-build-isolation --force-reinstall
+pip install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.3
+pip install --upgrade vllm
