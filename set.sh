1d19a63c8eb0ab16aa5508ab150053b2af2595ff
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-11-05 14:54:36 +0800

    update set up file for vLLM inference pipeline

--- set.sh
+++
@@ -1,9 +0,0 @@
-pip install nb-mermaid astor
-npm install @mermaid-js/mermaid-cli
-# brew install inkscape imagemagick
-# curl -fsSL https://d2lang.com/install.sh | sh -s --
-pip install --upgrade transformers trl huggingface_hub datasets accelerate bitsandbytes peft deepspeed
-MAX_JOBS=4 pip install flash-attn -U --no-build-isolation --force-reinstall
-pip install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.3
-pip install --upgrade vllm
-pip install --upgrade jupyterlab
