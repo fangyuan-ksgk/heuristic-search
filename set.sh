f085e4ba372b95889e2a058605d8b969a85cd704
FangyuanYu <fangyuan.yu@gmail.com>
2024-11-05 14:18:04 +0800

    update settings

--- set.sh
+++ set.sh
@@ -5,3 +5,4 @@ npm install @mermaid-js/mermaid-cli
 pip install --upgrade transformers trl huggingface_hub datasets accelerate bitsandbytes peft vllm deepspeed
 MAX_JOBS=4 pip install flash-attn -U --no-build-isolation --force-reinstall
 pip install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.3
+pip install --upgrade jupyterlab
