1a462ccc941314527866371d251ef9c24db557fa
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-11-05 15:24:12 +0800

    update set up file for linux environment d2 diagram visualization

--- set.sh
+++ set.sh
@@ -2,6 +2,7 @@ pip install nb-mermaid astor
 npm install @mermaid-js/mermaid-cli
 # brew install inkscape imagemagick
 curl -fsSL https://d2lang.com/install.sh | sh -s --
+apt-get install libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libxkbcommon0 libatspi2.0-0 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libpango-1.0-0 libcairo2 libasound2
 pip install --upgrade transformers trl huggingface_hub datasets accelerate bitsandbytes peft deepspeed sentence_transformers
 MAX_JOBS=4 pip install flash-attn -U --no-build-isolation --force-reinstall
 pip install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.3
